41958df42c9859499f2a26fa20f1cb3f1459255b
魔法少年李小狼 <skysly@yeah.net>
2022-03-12 08:49:51 -0800

    Add files via upload

---
+++ 服务端/server_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export ROSLISP_PACKAGE_DIRECTORIES='/home/rinspace/Codes/Ros/myfirst_ws/devel/share/common-lisp'
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/rinspace/Codes/Ros/myfirst_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/rinspace/Codes/Ros/myfirst_ws/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/noetic/bin:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/rinspace/.local/bin:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/rinspace/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/rinspace/Codes/Ros/myfirst_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/rinspace/Codes/Ros/myfirst_ws/build'
+export ROS_PACKAGE_PATH="/home/rinspace/Codes/Ros/myfirst_ws/src:$ROS_PACKAGE_PATH"
